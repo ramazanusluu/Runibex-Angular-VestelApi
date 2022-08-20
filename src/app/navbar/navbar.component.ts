2d5b4f6f82abf491aa9d18f2563222b99183c3b1
Ramazan USLU <ruslu1720@gmail.com>
2022-08-20 22:46:22 +0300

    Navbar uzerinde bulunan Tum Urunler butonua tiklanildiginda Sidebar Menunun acilmasi gerceklestirildi

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  open: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  isOpen() {
+    this.open = false ? (this.open === true) : (this.open === false);
+    console.log(this.open);
   }
-
 }
