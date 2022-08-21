99fc8fab120873205178dbb0b5813863137aec11
Ramazan USLU <ruslu1720@gmail.com>
2022-08-21 18:43:54 +0300

    Product verileri cekildi ve console yazdirildi

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  product: any;
+  constructor(
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.http
+        .get(
+          'https://store.vrunibex.com/mobile2/mbProduct/ProductList?CategoryID=' +
+            params['product_id']
+        )
+        .subscribe((data) => {
+          this.product = data;
+          console.log(this.product);
+        });
+    });
   }
-
 }
