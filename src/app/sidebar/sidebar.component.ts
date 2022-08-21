bff25cf155dbb50abc9e49284546f29193254b55
Ramazan USLU <ruslu1720@gmail.com>
2022-08-21 03:09:48 +0300

    Sidebar userinden gerceklesecek yonlendirme icin category ve subCategory'ler listelendi

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+  category: any = [];
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get('https://store.vrunibex.com/mobile2/mbProduct/CategoryList')
+      .subscribe((data) => {
+        this.category = data;
+        console.log(this.category);
+      });
   }
-
 }
