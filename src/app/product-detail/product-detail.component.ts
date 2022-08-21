c808aa8e05c60eb56665c30ef344c1ae51f4eb62
Ramazan USLU <ruslu1720@gmail.com>
2022-08-21 21:02:06 +0300

    Product detail icin product id'si yakalandi

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  productDetail: any;
+  constructor(
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      console.log(params['product']);
+    });
   }
-
 }
